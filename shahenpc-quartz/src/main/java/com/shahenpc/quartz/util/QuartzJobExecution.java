519a7da7c7f2cab1deedeefaaf7cef3d5b2e556e
黄涛 <472280437@qq.com>
2022-06-29 09:56:18 +0800

    INIT

---
+++ shahenpc-quartz/src/main/java/com/shahenpc/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.shahenpc.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import com.shahenpc.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
