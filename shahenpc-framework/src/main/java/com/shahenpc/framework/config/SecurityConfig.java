34022556c9252eaf6e31210d8c5b7d18e3372a10
黄涛 <472280437@qq.com>
2022-08-23 19:09:03 +0800

    登录国密SM3身份校验

--- shahenpc-framework/src/main/java/com/shahenpc/framework/config/SecurityConfig.java
+++ shahenpc-framework/src/main/java/com/shahenpc/framework/config/SecurityConfig.java
@@ -144,6 +144,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+        auth.authenticationProvider(new CustomLoginAuthenticationProvider(userDetailsService));
+//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 }
